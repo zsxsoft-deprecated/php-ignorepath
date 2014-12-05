9123be7b05841a32c1c3114157b119a50991b833
zsxsoft <zsxsoft@outlook.com>
2014-12-05 22:21:45 +0800

    RegExp

--- parser.php
+++ parser.php
@@ -45,18 +45,18 @@ class PathParser {
 		$array = explode("\n", $ruleText);
 		if ($this->debug) { var_dump($array); }
 		for ($i = 0; $i < count($array); $i++) {
-			echo $array[$i][0] . "\n";
+			//echo $array[$i][0] . "\n";
 			switch ($array[$i][0]) {
 				case "#":
 					// Ignore
 					break;
 				case ":":
 					// RegExp
-					array_push($this->_regExList, substr($array[$i], strpos($array[$i], "|") + 1));
+					$this->buildRegExpList($array[$i], $this->_regExList);
 					break;
 				case '!':
 					if ($array[$i][1] == ':') {
-						array_push($this->_regExList, substr(substr($array[$i], strpos($array[$i], "|") + 1), 1));
+						$this->buildRegExpList($array[$i], $this->_regExExcludeList);
 					} else {
 						$this->buildTree(0, explode("/", substr($array[$i], 1)), $this->_ruleExcludeTree);
 					}
@@ -78,7 +78,7 @@ class PathParser {
 	 * @param $tree
 	 * @return bool
 	 */
-	public function buildTree($deep, $array, &$tree) {
+	private function buildTree($deep, $array, &$tree) {
 		if ($this->debug) { echo 'deep = ' . $deep . ', $array[$deep] = ' . $array[$deep] . "\n";}
 		if ($deep == count($array)) return;
 		$val = $array[$deep];
@@ -89,6 +89,25 @@ class PathParser {
 		}
 		$childTree = &$tree[$val];
 		$this->buildTree($deep + 1, $array, $childTree);
+		return true;
+	}
+
+
+	/**
+	 * buildRegExpList
+	 * @param $string
+	 * @param $list
+	 * @return bool
+	 */
+	private function buildRegExpList($string, &$list) {
+
+		$string = substr($string, ($string[0] == '!' ? 2 : 1));
+		$pattern = substr($string, 0, strpos($string, "|"));
+		$main = substr($string, strpos($string, "|") + 1);
+		$regex = "/" . $main . "/" . $pattern;
+		array_push($list, $regex);
+		return true;
+
 	}
 
 
@@ -132,7 +151,11 @@ class PathParser {
 	 * @param $path
 	 * @return bool
 	 */
-	public function _checkRegExp($path) {
+	public function _checkRegExp($path, $array) {
+		foreach ($array as $key => $value) {
+			echo $value . "\n";  
+			if (preg_match($value, $path)) return true;
+		}
 		return false;
 	}
 
@@ -143,7 +166,7 @@ class PathParser {
 	 * @return bool
 	 */
 	public function checkPath($path) {
-		
+
 		// False means exclude
 		// True means include
 
